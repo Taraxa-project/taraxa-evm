de0359c3da42541cfa341befe73979159cba952a
John Doe <compuktor@Johns-MacBook-Pro.local>
2019-02-13 20:45:23 +0000

    fixed a warning

--- cmd/utils/customflags_test.go
+++ cmd/utils/customflags_test.go
@@ -23,10 +23,10 @@ import (
 )
 
 func TestPathExpansion(t *testing.T) {
-	user, _ := user.Current()
+	usr, _ := user.Current()
 	tests := map[string]string{
 		"/home/someuser/tmp": "/home/someuser/tmp",
-		"~/tmp":              user.HomeDir + "/tmp",
+		"~/tmp":              usr.HomeDir + "/tmp",
 		"~thisOtherUser/b/":  "~thisOtherUser/b",
 		"$DDDXXX/a/b":        "/tmp/a/b",
 		"/a/b/":              "/a/b",
