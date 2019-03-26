d5af04b544c2f8f3832ac68868131132aa27adf8
Oleh L <02p01r@gmail.com>
2019-03-26 23:07:09 +0200

    misc

--- lib_cpp/src/lib.cpp
+++ lib_cpp/src/lib.cpp
@@ -1,18 +1,13 @@
 #include "taraxa_evm_cgo.h"
 #include "taraxa_evm/cgo_bridge.hpp"
-#include <iostream>
 
 using namespace std;
 
 namespace taraxa_evm::cgo_bridge {
 
     string run(const string &jsonConfig) {
-        using namespace std;
-        cout << jsonConfig << endl;
         auto cStr = const_cast<char *>(jsonConfig.c_str());
-        string result = RunEvm(cStr);
-        cout << result << endl;
-        return result;
+        return RunEvm(cStr);
     }
 
 }
