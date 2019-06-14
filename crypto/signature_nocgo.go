70a138e274526bac3c90d695518e4f10c1ea0315
Oleh L <02p01r@gmail.com>
2019-06-14 21:52:18 +0300

    added cgo disabling ability for secp256k1 dependency

--- crypto/signature_nocgo.go
+++ crypto/signature_nocgo.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build nacl js nocgo
+// +build nacl js nocgo secp256k1_no_cgo
 
 package crypto
 
