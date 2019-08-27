5a2a9a629b4622095bf05b180199cab0bf384577
Oleh L <02p01r@gmail.com>
2019-08-28 00:55:11 +0300

    -

--- tests_perf/sandbox.py
+++ tests_perf/sandbox.py
@@ -17,7 +17,7 @@ state_db = rocksdb.DB(f'/workspace/data/ethereum_state_mainnet_rocksdb',
                       rocksdb.Options(create_if_missing=True),
                       read_only=True)
 
-for k, v in block_db.iteritems(from_block=4500000):
+for k, v in block_db.iteritems(from_block=from_block):
     state_root = bytes.fromhex(v['stateRoot'].replace('0x', ''))
     if not state_db.get(state_root):
         break
