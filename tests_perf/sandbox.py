85ea74a9e21b8a2ceadc2b004f85850eed158935
Oleh L <02p01r@gmail.com>
2019-08-28 00:13:43 +0300

    -

---
+++ tests_perf/sandbox.py
@@ -0,0 +1,9 @@
+import rocksdb
+
+from apps.blockchain_data import BlockDB
+
+block_db = BlockDB(
+    rocksdb.DB('/workspace/data/ethereum_blockchain_mainnet_rocksdb', rocksdb.Options(create_if_missing=True)))
+
+for k, v in block_db.iteritems():
+    print(f"{k} : {v}")
