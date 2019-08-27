7a9031182be58667b90b2a3e0deaa7035ab5e25e
Oleh L <02p01r@gmail.com>
2019-08-27 22:39:47 +0300

    WIP

--- tests_perf/docker_env_shell.sh
+++ tests_perf/docker_env_shell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t tmp - <"$(dirname "$0")"/Dockerfile && docker run -it -v /workspace:"$1" tmp
+docker build -t tmp - <"$(dirname "$0")"/Dockerfile && docker run -it -v /workspace:/workspace tmp
