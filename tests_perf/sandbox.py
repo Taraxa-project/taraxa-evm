61242d326674724a4d7e96047340bfdd444da4d5
Oleh L <02p01r@gmail.com>
2019-08-28 00:51:32 +0300

    -

--- tests_perf/sandbox.py
+++ tests_perf/sandbox.py
@@ -2,7 +2,7 @@ import rocksdb
 
 from apps.blockchain_data import BlockDB
 
-from_block = 2176170
+from_block = 4500000
 
 block_db = BlockDB(
     rocksdb.DB('/workspace/data/ethereum_blockchain_mainnet_rocksdb',
@@ -10,9 +10,15 @@ block_db = BlockDB(
                read_only=True))
 
 block_db_to = BlockDB(
-    rocksdb.DB(f'/workspace/data/ethereum_blockchain_mainnet_rocksdb_{from_block}_{block_db.max_block_num()}',
+    rocksdb.DB(f'/workspace/data/ethereum_blockchain_mainnet_rocksdb_{from_block}_{from_block + 1000000}',
                rocksdb.Options(create_if_missing=True)))
 
-for k, v in block_db.iteritems(from_block=from_block):
-    block_db_to.put_block(v)
+state_db = rocksdb.DB(f'/workspace/data/ethereum_blockchain_mainnet_rocksdb_ethereum_state_mainnet_rocksdb',
+                      rocksdb.Options(create_if_missing=True),
+                      read_only=True)
+
+for k, v in block_db.iteritems(from_block=4500000):
+    state_root = bytes.fromhex(v['stateRoot'].replace('0x', ''))
+    if not state_db.get(state_root):
+        break
     print(k)
