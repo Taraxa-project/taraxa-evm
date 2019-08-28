db858fe43a814781c166f0b543dceb941a718beb
Oleh L <02p01r@gmail.com>
2019-08-28 03:19:06 +0300

    -

--- misc/index.go
+++ misc/index.go
@@ -1,14 +1,19 @@
 package misc
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/Taraxa-project/taraxa-evm/ethdb"
+	"github.com/Taraxa-project/taraxa-evm/rlp"
 	"github.com/Taraxa-project/taraxa-evm/taraxa/db/rocksdb"
 	"github.com/Taraxa-project/taraxa-evm/taraxa/util"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
-	"math/big"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/trie"
 )
+
+var emptyCodeHash = crypto.Keccak256(nil)
 
 func DumpStateRocksdb(db_path_source, root_str string) {
 	fmt.Println("foo")
@@ -18,24 +23,38 @@ func DumpStateRocksdb(db_path_source, root_str string) {
 		ReadOnly: true,
 	}).NewInstance()
 	util.PanicIfPresent(err0)
-	state_source, err1 := state.New(root, state.NewDatabase(&dbAdapter{rocksdb_source}))
+	db_source := state.NewDatabase(&dbAdapter{rocksdb_source})
+	acc_trie_source, err1 := db_source.OpenTrie(root)
 	util.PanicIfPresent(err1)
 	db_dest := ethdb.NewMemDatabase()
 	state_dest, err2 := state.New(common.Hash{}, state.NewDatabase(&dbAdapter{db_dest}))
 	util.PanicIfPresent(err2)
-	dump := state_source.RawDump(false, false, false)
-	for addr, acc := range dump.Accounts {
-		fmt.Println(addr.Hex())
+	fmt.Println("bar")
+	for acc_itr := trie.NewIterator(acc_trie_source.NodeIterator(nil)); acc_itr.Next(); {
+		var acc state.Account
+		err := rlp.DecodeBytes(acc_itr.Value, &acc)
+		util.PanicIfPresent(err)
+		addr := common.BytesToAddress(acc_trie_source.GetKey(acc_itr.Key))
+		addrHash := crypto.Keccak256Hash(addr[:])
+		state_dest.SetBalance(addr, acc.Balance)
 		state_dest.SetNonce(addr, acc.Nonce)
-		balance, parsed := new(big.Int).SetString(acc.Balance, 10)
-		util.Assert(parsed)
-		state_dest.SetBalance(addr, balance)
-		state_dest.SetCode(addr, common.Hex2Bytes(acc.Code))
-		storage := make(map[common.Hash]common.Hash, len(acc.Storage))
-		for loc, val := range acc.Storage {
-			storage[loc] = common.HexToHash(val)
+		var code []byte
+		if !bytes.Equal(acc.CodeHash, emptyCodeHash) {
+			var err error
+			code, err = db_source.ContractCode(addrHash, common.BytesToHash(acc.CodeHash))
+			util.PanicIfPresent(err)
 		}
-		state_dest.SetStorage(addr, storage)
+		state_dest.SetCode(addr, code)
+		storage_trie, err1 := db_source.OpenStorageTrie(addrHash, root)
+		util.PanicIfPresent(err1)
+		for storage_itr := trie.NewIterator(storage_trie.NodeIterator(nil)); storage_itr.Next(); {
+			_, content, _, err := rlp.Split(storage_itr.Value)
+			util.PanicIfPresent(err)
+			state_dest.SetState(
+				addr,
+				common.BytesToHash(storage_trie.GetKey(storage_itr.Key)),
+				common.BytesToHash(content))
+		}
 	}
 	fmt.Println("baz")
 	root_dest, err3 := state_dest.Commit(false)
