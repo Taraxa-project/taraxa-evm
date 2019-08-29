e0aeb661a7f2e36cdc8e087366d08c9b3c7c2db2
Oleh L <02p01r@gmail.com>
2019-08-29 13:44:28 +0300

    -

--- tests_perf/sandbox.py
+++ tests_perf/sandbox.py
@@ -13,15 +13,15 @@ block_db_to = BlockDB(
     rocksdb.DB(f'/workspace/data/ethereum_blockchain_mainnet_{from_block}_{from_block + 1000000}_rocksdb',
                rocksdb.Options(create_if_missing=True)))
 
-state_db = rocksdb.DB(f'/workspace/data/ethereum_state_mainnet_rocksdb',
-                      rocksdb.Options(create_if_missing=True),
-                      read_only=True)
+# state_db = rocksdb.DB(f'/workspace/data/ethereum_state_mainnet_rocksdb',
+#                       rocksdb.Options(create_if_missing=True),
+#                       read_only=True)
 
 for k, v in block_db.iteritems(from_block=from_block):
-    # block_db_to.put_block(v)
-    state_root = v['stateRoot']
-    state_root_bytes = bytes.fromhex(state_root.replace('0x', ''))
-    if not state_db.get(state_root_bytes):
-        break
+    block_db_to.put_block(v)
+    # state_root = v['stateRoot']
+    # state_root_bytes = bytes.fromhex(state_root.replace('0x', ''))
+    # if not state_db.get(state_root_bytes):
+    #     break
+    # print(state_root)
     print(k)
-    print(state_root)
