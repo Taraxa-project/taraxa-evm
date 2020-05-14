09a0dab9b4d5a675dbb734f1b90a84a424141c60
Oleh L <02p01r@gmail.com>
2020-05-14 20:20:23 +0400

    dbg

--- taraxa/C/state.go
+++ taraxa/C/state.go
@@ -5,7 +5,6 @@ package main
 //#include <rocksdb/c.h>
 import "C"
 import (
-	"fmt"
 	"github.com/Taraxa-project/taraxa-evm/common"
 	"github.com/Taraxa-project/taraxa-evm/core/types"
 	"github.com/Taraxa-project/taraxa-evm/core/vm"
@@ -50,41 +49,24 @@ func taraxa_evm_state_API_New(
 		CurrStateRoot              common.Hash
 		StateTransitionCacheOpts   state_transition.CacheOpts
 	}
-	fmt.Println("1")
 	dec_rlp(params_enc, &params)
-	fmt.Println("PARAMS:", params)
-	fmt.Println("11")
 	self := new(state_API)
-	fmt.Println("111")
 	rocksdb := gorocksdb.NewDBFromNative(unsafe.Pointer(params.RocksDBPtr))
-	fmt.Println("1111")
 	var columns state_db_rocksdb.Columns
-	fmt.Println("11111")
 	for i, ptr := range params.RocksDBColumnFamilyHandles {
-		fmt.Println("111111")
 		columns[i] = gorocksdb.NewNativeColumnFamilyHandle1(unsafe.Pointer(ptr))
-		fmt.Println("1111111")
 	}
-	fmt.Println("11111111")
 	self.db.Init(rocksdb, columns)
-	fmt.Println("1111111111")
 	self.get_blk_hash_C = *(*C.taraxa_evm_GetBlockHash)(unsafe.Pointer(params.GetBlockHash))
-	fmt.Println("11111111111")
 	self.API.Init(&self.db, self.blk_hash,
 		params.ChainConfig, params.CurrBlkNum, params.CurrStateRoot, params.StateTransitionCacheOpts)
-	fmt.Println("111111111111")
 	defer util.LockUnlock(&state_API_alloc_mu)()
-	fmt.Println("1111111111111")
 	lastpos := len(state_API_available_ptrs) - 1
-	fmt.Println("11111111111111")
 	assert.Holds(lastpos >= 0)
-	fmt.Println("111111111111111")
 	ptr := state_API_available_ptrs[lastpos]
-	fmt.Println("1111111111111111")
+	assert.Holds(state_API_instances[ptr] == nil)
 	state_API_available_ptrs = state_API_available_ptrs[:lastpos]
-	fmt.Println("11111111111111111")
 	state_API_instances[ptr] = self
-	fmt.Println("111111111111111111")
 	return C.taraxa_evm_state_API_ptr(ptr)
 }
 
@@ -95,7 +77,7 @@ func taraxa_evm_state_API_Free(
 ) {
 	defer handle_err(cb_err)
 	defer util.LockUnlock(&state_API_alloc_mu)()
-	assert.Holds(state_API_instances[ptr] == nil)
+	assert.Holds(state_API_instances[ptr] != nil)
 	state_API_instances[ptr], state_API_available_ptrs = nil, append(state_API_available_ptrs, state_API_ptr(ptr))
 }
 
