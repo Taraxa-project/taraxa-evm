c9427939aa35dd28f2fdf78010c200f665911c03
Oleh L <02p01r@gmail.com>
2020-05-27 00:07:47 +0400

    dbg

--- taraxa/C/state.go
+++ taraxa/C/state.go
@@ -154,6 +154,7 @@ func taraxa_evm_state_API_Historical_GetAccountStorage(
 	dump_json(params)
 	self := state_API_instances[state_API_ptr(ptr)]
 	ret := self.Historical.AtBlock(params.BlkNum).GetAccountStorageRaw(&params.Addr, &params.Key)
+	fmt.Println("ret:", ret)
 	call_bytes_cb(ret, cb)
 }
 
