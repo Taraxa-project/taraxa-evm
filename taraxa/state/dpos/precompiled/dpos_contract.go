4ee1856b30dd3587cfc72f57e02794d35e9746fd
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-05-19 11:19:36 -0700

    add undelegations count into solidity interface

--- taraxa/state/dpos/precompiled/dpos_contract.go
+++ taraxa/state/dpos/precompiled/dpos_contract.go
@@ -541,7 +541,7 @@ func (self *Contract) Run(ctx vm.CallFrame, evm *vm.EVM) ([]byte, error) {
 			fmt.Println("Unable to parse getValidator input args: ", err)
 			return nil, err
 		}
-		result, err := self.getValidatorInfo(evm.GetBlock().Number, args)
+		result, err := self.getValidator(evm.GetBlock().Number, args)
 		if err != nil {
 			return nil, err
 		}
@@ -1378,7 +1378,7 @@ func (self *Contract) setCommission(ctx vm.CallFrame, block types.BlockNum, args
 }
 
 // Returns single validator object
-func (self *Contract) getValidatorInfo(block types.BlockNum, args sol.ValidatorAddressArgs) (sol.DposInterfaceValidatorBasicInfo, error) {
+func (self *Contract) getValidator(block types.BlockNum, args sol.ValidatorAddressArgs) (sol.DposInterfaceValidatorBasicInfo, error) {
 	var result sol.DposInterfaceValidatorBasicInfo
 	validator := self.validators.GetValidator(&args.Validator)
 	if validator == nil {
@@ -1397,6 +1397,7 @@ func (self *Contract) getValidatorInfo(block types.BlockNum, args sol.ValidatorA
 	result.CommissionReward = validator_rewards.CommissionRewardsPool
 	result.LastCommissionChange = validator.LastCommissionChange
 	result.Owner = self.validators.GetValidatorOwner(&args.Validator)
+	result.UndelegationsCount = validator.UndelegationsCount
 	result.TotalStake = validator.TotalStake
 	result.Endpoint = validator_info.Endpoint
 	result.Description = validator_info.Description
