61db3df5bca3fd4d8a0ed8bbfb9594cc47696531
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-07-25 14:04:53 -0700

    implement contract getter functions + additional unit tests

--- taraxa/state/contracts/slashing/solidity/slashing_contract_interface.sol
+++ taraxa/state/contracts/slashing/solidity/slashing_contract_interface.sol
@@ -10,7 +10,7 @@ interface SlashingInterface {
 
     // Commit double voting malicious behaviour proof
     function commitDoubleVotingProof(
-        address author,    // proof author
+        address proof_author,    // proof author
         address validator, // malicious validator
         bytes memory vote1,
         bytes memory vote2
@@ -19,7 +19,54 @@ interface SlashingInterface {
     /**
      * @notice Returns true if validator is currently jailed due to malicious behaviour, otherwise false
      *
-     * @param validator     validator account address
+     * @param validator validator's address
      **/
     function isJailed(address validator) external view returns (bool);
+
+    struct JailInfo {
+        uint256 jail_block;  // block until which is validator jailed
+        bool is_jailed;      // flag if validator is currently jailed
+        uint32 proofs_count; // number of malicious behaviour proofs
+    }
+
+    /**
+     * @notice Returns validator's jail info - jail_block == 0 in case 
+     *
+     * @param validator validator's address
+     **/
+    function getJailInfo(address validator) external view returns (uint256);
+
+    struct MaliciousValidator {
+        address validator;
+        JailInfo jail_info;
+    }
+
+    /**
+     * @notice Returns list of malicious validators
+     *
+     * @return validators Batch of N malicious validators
+     **/
+    function getMaliciousValidators()
+        external
+        view
+        returns (MaliciousValidator[] memory validators);
+
+    struct DoubleVotingProof {
+        address proof_author; // author of the proof
+        uint256 block;
+        string vote1_hash; 
+        string vote2_hash; 
+        string tx_hash; 
+    }
+
+    /**
+     * @notice Returns list of malicious validators
+     * @param validator validator's address
+     *
+     * @return proofs double voting proofs
+     **/
+    function getDoubleVotingProofs(address validator)
+        external
+        view
+        returns (DoubleVotingProof[] memory proofs);
 }
