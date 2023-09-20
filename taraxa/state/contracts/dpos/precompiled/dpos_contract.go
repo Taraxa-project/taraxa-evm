66f5924bc1cbd633d965b2b0c3453c90778ed364
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-09-20 11:18:04 -0700

    fix delegate function bug

--- taraxa/state/contracts/dpos/precompiled/dpos_contract.go
+++ taraxa/state/contracts/dpos/precompiled/dpos_contract.go
@@ -772,12 +772,12 @@ func (self *Contract) delegate(ctx vm.CallFrame, block types.BlockNum, args dpos
 
 	state, state_k := self.state_get(args.Validator[:], BlockToBytes(block))
 	if state == nil {
-		old_state := self.state_get_and_decrement(args.Validator[:], BlockToBytes(validator.LastUpdated))
 		state = new(State)
-		state.RewardsPer1Stake = old_state.RewardsPer1Stake
+		state.RewardsPer1Stake = big.NewInt(0)
 		if validator.TotalStake.Cmp(big.NewInt(0)) > 0 {
 			state.RewardsPer1Stake.Add(state.RewardsPer1Stake, self.calculateRewardPer1Stake(validator_rewards.RewardsPool, validator.TotalStake))
 		}
+
 		validator_rewards.RewardsPool = big.NewInt(0)
 		validator.LastUpdated = block
 		state.Count++
