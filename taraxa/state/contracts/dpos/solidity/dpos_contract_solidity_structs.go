4fdd35c981f9e544666b6c754aaf73e254acd1b7
Jakub Fornadel <jakub.fornadel@gmail.com>
2023-10-20 11:42:32 -0700

    small fixes

--- taraxa/state/contracts/dpos/solidity/dpos_contract_solidity_structs.go
+++ taraxa/state/contracts/dpos/solidity/dpos_contract_solidity_structs.go
@@ -91,7 +91,7 @@ type RegisterValidatorArgs struct {
 	Validator   common.Address
 	Proof       []byte
 	VrfKey      []byte
-	BlsKey      []byte
+	BlsKey      []byte `rlp:"nil"`
 	Commission  uint16
 	Description string
 	Endpoint    string
