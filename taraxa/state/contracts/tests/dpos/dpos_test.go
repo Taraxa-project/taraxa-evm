b639e1fa0e6b712542ed574859ded23ad8ecea85
Matus Kysel <matuskysel1@gmail.com>
2024-03-15 21:24:19 +0100

    fix the test

--- taraxa/state/contracts/tests/dpos/dpos_test.go
+++ taraxa/state/contracts/tests/dpos/dpos_test.go
@@ -312,8 +312,7 @@ func TestRedelegate(t *testing.T) {
 	test.ExecuteAndCheck(validator1_owner, big.NewInt(0), test.Pack("reDelegate", validator1_addr, validator1_addr, DefaultMinimumDeposit), dpos.ErrSameValidator, util.ErrorString(""))
 
 	// Check for negative and zero redelegation
-	test.ExecuteAndCheck(validator1_owner, big.NewInt(0), test.Pack("reDelegate", validator1_addr, validator1_addr, 0), dpos.ErrInvalidRedelegation, util.ErrorString(""))
-	test.ExecuteAndCheck(validator1_owner, big.NewInt(0), test.Pack("reDelegate", validator1_addr, validator1_addr, -5), dpos.ErrInvalidRedelegation, util.ErrorString(""))
+	test.ExecuteAndCheck(validator1_owner, big.NewInt(0), test.Pack("reDelegate", validator1_addr, validator2_addr, big.NewInt(0)), dpos.ErrInvalidRedelegation, util.ErrorString(""))
 
 	test.CheckContractBalance(totalBalance)
 }
